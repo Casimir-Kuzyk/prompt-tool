4f47162743cde2d2ad16e29a7256dfb0f6cc47de
Casimir-Kuzyk <casimirkuzyk@gmail.com>
2023-04-03 11:15:56 -0700

    first test of actual tool

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # prompt-tool
+
+Instructions:
+1. install commander framework  --  npm install commander
+2. install path  --  npm install path
+3. install fs  --  npm install fs
+4. install the gpt tool  --  npm install gpt-prompt-tool
--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const { Command } = require('commander');
+const fs = require('fs');
+const path = require('path');
 
 const program = new Command();
 
 program
-    .command('add')
-    .description('add 2 numbers')
-    .action(iswds)
+    .arguments('<files...>')
+    .description('takes all the requested files in the folder and compiles them into a file called compiled-files, and also copies to your clipboard')
+    .action(combiner)
 
-program.parse(process.argv);
+function combiner(files){
+        // Combine text from input files
+        const combinedText = files.reduce((acc, file) => {
+            const filePath = path.resolve(process.cwd(), file);
+            const fileText = fs.readFileSync(filePath, 'utf-8');
+            const extension = path.extname(filePath);
+            const fileType = extension ? ` from a ${extension.slice(1)} file` : '';
+            return `${acc}\n\n// The below text is${fileType}\n${fileText}`;
+            }, '');
+        
+            // Write combined text to output file
+            const outputFilePath = path.resolve(process.cwd(), 'combined-files.txt');
+            fs.writeFileSync(outputFilePath, combinedText);
+            console.log(`Successfully combined text from ${files.length} files into ${outputFilePath}`);
+        };
 
-
-function iswds(string){
-    console.log('hey sup tester');
-    return string === 'hello';
-}
-
-
-
-module.exports = iswds
+module.exports = combiner;
