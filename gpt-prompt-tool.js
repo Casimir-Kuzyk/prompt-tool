2f1d74df217beeac8e9c224bf96f91812a135ff1
Casimir-Kuzyk <casimirkuzyk@gmail.com>
2023-04-04 12:01:32 -0700

    copy paste package attempt 2

--- gpt-prompt-tool.js
+++ gpt-prompt-tool.js
@@ -26,15 +26,17 @@ function combiner(files){
             const fileText = fs.readFileSync(filePath, 'utf-8');
             const extension = path.extname(filePath);
             const fileType = extension ? ` from a ${extension.slice(1)} file` : '';
-            return `${acc}\n\n// The below text is${fileType}\n${fileText}`;
+            return `${acc}\n\n//// The below text is${fileType}\n${fileText}`;
             }, '');
         
             // Write combined text to output file
             //const outputFilePath = path.resolve(process.cwd(), 'combined-files.txt');
             const outputFilePath = path.resolve(require('os').homedir(), 'Desktop', 'combined-files.txt');
             fs.writeFileSync(outputFilePath, combinedText);
-            copyPaste.copy(combinedText)
-            console.log(`Successfully combined text from ${files.length} files into ${outputFilePath} and copied it to the clipboard`);
+            copyPaste.copy(combinedText, () => {
+                console.log(`Successfully combined text from ${files.length} files into ${outputFilePath} and copied it to the clipboard`);
+            });
+
         };
 
 program.parse(process.argv);
